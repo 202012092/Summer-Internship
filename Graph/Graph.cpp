fb28a5868ce93205a10ee535a10d91a66daa7a7a
202012092 <83826260+202012092@users.noreply.github.com>
2021-08-07 16:22:32 +0530

    Update Graph.cpp

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -1,46 +1,94 @@
 #include <iostream>
 #include <vector>
+#include<map>
+#include<list>
 
 using namespace std;
 
-class Graph {
-
+class Graph
+{
 public:
+    int V;
+	map<int, bool> visited;
+    map<int, list<int>> adj;
+    list<int>* adj1;
 
-    void addEdge(vector<int> adj[], const int& u, const int& v)
-    {
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
+    Graph(int V);
 
-    void printGraph(vector<int> adj[], const int& V)
-    {
-        for (int v = 0; v < V; ++v)
-        {
-            cout << "\n Adjacency list of vertex "
-                << v << "\n head ";
-            for (auto x : adj[v])
-                cout << "-> " << x;
-            printf("\n");
-        }
-    }
+	void addEdge(int v, int w);
+
+	void DFS(int v);
+
+    void BFS(int s);
 };
 
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj1 = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w)
+{
+	adj[v].push_back(w);
+}
+
+void Graph::DFS(int v)
+{
+	visited[v] = true;
+	cout << v << " ";
+
+	list<int>::iterator i;
+	for (i = adj[v].begin(); i != adj[v].end(); ++i)
+		if (!visited[*i])
+			DFS(*i);
+}
+
+void Graph::BFS(int s)
+{
+    bool* visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+
+    list<int> queue;
+
+    visited[s] = true;
+    queue.push_back(s);
+
+    list<int>::iterator i;
+
+    while (!queue.empty())
+    {
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+        for (i = adj[s].begin(); i != adj[s].end(); ++i)
+        {
+            if (!visited[*i])
+            {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
+}
 int main()
 {
-    const int V = 5;
+    Graph g(4);
+	g.addEdge(0, 1);
+	g.addEdge(0, 2);
+	g.addEdge(1, 2);
+	g.addEdge(2, 0);
+	g.addEdge(2, 3);
+	g.addEdge(3, 3);
 
-    vector<int> adj[V];
+	cout << "Following is Depth First Traversal"
+		" (starting from vertex 2) \n";
+	g.DFS(2);
+    cout << endl;
+    cout << "Following is Breadth First Traversal "
+        << "(starting from vertex 2) \n";
+    g.BFS(2);
 
-    Graph g;
-
-    g.addEdge(adj, 0, 1);
-    g.addEdge(adj, 0, 4);
-    g.addEdge(adj, 1, 2);
-    g.addEdge(adj, 1, 3);
-    g.addEdge(adj, 1, 4);
-    g.addEdge(adj, 2, 3);
-    g.addEdge(adj, 3, 4);
-    g.printGraph(adj, V);
-    return 0;
+	return 0;
 }
